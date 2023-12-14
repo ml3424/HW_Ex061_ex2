475dc2a5105860c961e8eae79581f817aaa63a9d
ml3424 <ml3424@bs.amalnet.k12.il>
2023-12-14 10:20:19 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hw_ex061_ex2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.hw_ex061_ex2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+    }
+
+    public void count_click(View view) {
+        counter ++;
+        btn.setText("This is a click number:" + counter);
+    }
+}
